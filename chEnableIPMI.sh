d31b3ce13e2f0c58199a6f79a6a2a47ec3c427de
yudianzhiyu <yudianzhiyu@gmail.com>
2015-06-16 11:55:12 +0800

    Update chEnableIPMI.sh
    send yes

--- chEnableIPMI.sh
+++ chEnableIPMI.sh
@@ -6,16 +6,18 @@ function chbootmode()
         ip=$1
         expect  -c "
                 spawn  ssh -f   root@${ip} racadm config -g cfgIpmiLan -o cfgIpmiLanEnable 1  ;
-                set timeout 31;
+                set timeout 9;
                 expect {
+                        \"*continue*\"  { send  \"yes\r\"   ; exp_continue  } 
                         \"*assword*\" {send  \"calvin\r\" ; exp_continue}
                 }
         "
         echo  
         expect -c "
                 spawn  ssh  -f  root@${ip} racadm jobqueue create BIOS.Setup.1-1 ;
-                set timeout 30;
+                set timeout 9;
                 expect {
+                        \"*continue*\"  { send  \"yes\r\"   ; exp_continue  } 
                         \"*assword*\" {send  \"calvin\r\"; exp_continue}
                 }
         "
